1f242539ff4ff50df3bcd3f314a1f037ddfad774
cac100 <cac100@juniata.edu>
2025-04-22 11:10:04 -0400

    added more content

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 const Ballet = document.querySelector("#Ballet");
-Ballet.onclick - function () {
+Ballet.onclick = function () {
     alert("You selected Ballet!")
-}
+    }
+
+const Modern = document.querySelector("#Modern");
+Modern.onclick = function () {
+    alert("You selected Modern!")
+    }
+
+const Jazz = document.querySelector("#Jazz");
+Jazz.onclick = function () {
+    alert("You selected Jazz!")
+    }
+
+const Lyrical = document.querySelector("#Lyrical");
+Lyrical.onclick = function () {
+    alert("You selected Lyrical!")
+    }
+
+const Contemporary = document.querySelector("#Contemporary");
+Contemporary.onclick = function () {
+    alert("You selected Contemporary!")
+    }
+
+const Tap = document.querySelector("#Tap");
+Tap.onclick = function () {
+    alert("You selected Tap!")
+    }
+
+const Hiphop = document.querySelector("#Hiphop");
+Hiphop.onclick = function () {
+    alert("You selected Hiphop!")
+    }
