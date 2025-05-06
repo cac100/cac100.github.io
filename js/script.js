f07214c92fb13d0858843272e277aceb637f7d83
cac100 <cac100@juniata.edu>
2025-05-06 11:43:27 -0400

    d

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ fullname.oninput = function(){
     };
 
     Ballet.onclick = function () {
-        if (heading.innerHTML === "Welcome to GDS Callie") {
-          alert ("Ballet was clicked and your name is Callie");
+        if (heading.innerHTML === "Welcome to GDS Elainea") {
+          alert ("Ballet was clicked and your name is Elainea");
         } else {
             alert ("Ballet was clicked!");
         }
